88e524f84a20b278c394788a13f1109b32e09d04
Thomas Johnson <tjohnson1106@gmail.com>
2017-10-12 18:03:49 -0400

    replaced keybord module with Platform module for cross-platform compatibility with show keyboard animations

--- app/components/Logo/Logo.js
+++ app/components/Logo/Logo.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
-import { View, Image, Text, Keyboard, Animated } from "react-native";
+import { View, Image, Text, Animated, Platform } from "react-native";
 
 import styles from "./styles";
 
+const ANIMATION_DURATION = 250;
+
 class Logo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.containerImageWidth = new Animated.Value(styles.$largeContainerSize);
+    this.imageWidth = new Animated.Value(styles.$largeImageSize);
+  }
   componentDidMount() {
+    let showListener = "keyboardWillShow";
+    let hideListener = "keyboardWillHide";
+    if (Platform.os === "android") {
+      let showListener = "keyboardDidShow";
+      let hideListener = "keyboardDidHide";
+    }
     this.keyboardShowListener = Keyboard.addListener(
       "keyboardWillShow",
       this.keyboardShow
@@ -21,24 +35,49 @@ class Logo extends Component {
   }
 
   keyboardShow = () => {
-    console.log("keyboard did show");
+    Animated.parallel([
+      Animated.timing(this.containerImageWidth, {
+        toValue: styles.$smallContainerSize,
+        duration: ANIMATION_DURATION
+      }),
+      Animated.timing(this.imageWidth, {
+        toValue: styles.$smallImageSize,
+        duration: ANIMATION_DURATION
+      })
+    ]).start();
   };
 
   keyboardHide = () => {
-    console.log("keyboard did hide");
+    Animated.parallel([
+      Animated.timing(this.containerImageWidth, {
+        toValue: styles.$largeContainerSize,
+        duration: ANIMATION_DURATION
+      }),
+      Animated.timing(this.imageWidth, {
+        toValue: styles.$largeImageSize,
+        duration: ANIMATION_DURATION
+      })
+    ]).start();
   };
 
   render() {
+    const containerImageStyle = [
+      styles.containerImage,
+      { width: this.containerImageWidth, height: this.containerImageWidth }
+    ];
+
+    const imageStyle = [styles.logo, { width: this.imageWidth }];
+
     return (
       <View style={styles.container}>
         <Animated.Image
           resizeMode="contain"
-          style={styles.containerImage}
+          style={containerImageStyle}
           source={require("./images/background.png")}
         >
-          <ImageAnimated.Image
+          <Animated.Image
             resizeMode="contain"
-            style={styles.logo}
+            style={imageStyle}
             source={require("./images/logo.png")}
           />
         </Animated.Image>
