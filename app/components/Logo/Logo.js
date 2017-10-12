a62ba435e538ff8fffd43d5da17a1d1d33ba1159
Thomas Johnson <tjohnson1106@gmail.com>
2017-10-12 16:31:24 -0400

    created keybaord listeners

--- app/components/Logo/Logo.js
+++ app/components/Logo/Logo.js
@@ -1,16 +1,51 @@
-import React from 'react';
-import { View, Image, Text } from 'react-native';
+import React, { Component } from "react";
+import { View, Image, Text, Keyboard, Animated } from "react-native";
 
-import styles from './styles';
+import styles from "./styles";
 
-const Logo = () => (
-    <View style={styles.container}>
-        <Image resizeMode="contain" style={styles.containerImage} source={require('./images/background.png')}>
-            <Image resizeMode="contain" style={styles.logo} source={require('./images/logo.png')} />
-        </Image>
+class Logo extends Component {
+  componentDidMount() {
+    this.keyboardShowListener = Keyboard.addListener(
+      "keyboardWillShow",
+      this.keyboardShow
+    );
+    this.keyboardHideListener = Keyboard.addListener(
+      "keyboardWill",
+      this.keyboardHide
+    );
+  }
+
+  componentWillUnmount() {
+    this.keyboardShowListener.remove();
+    this.keyboardHideListener.remove();
+  }
+
+  keyboardShow = () => {
+    console.log("keyboard did show");
+  };
+
+  keyboardHide = () => {
+    console.log("keyboard did hide");
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Animated.Image
+          resizeMode="contain"
+          style={styles.containerImage}
+          source={require("./images/background.png")}
+        >
+          <ImageAnimated.Image
+            resizeMode="contain"
+            style={styles.logo}
+            source={require("./images/logo.png")}
+          />
+        </Animated.Image>
         <Text style={styles.text}>Currency Converter</Text>
-    </View>
-
-);
+      </View>
+    );
+  }
+}
 
 export default Logo;
--- app/components/Logo/styles.js
+++ app/components/Logo/styles.js
@@ -6,6 +6,8 @@ const imageWidth = Dimensions.get("window").width / 2;
 export default EStyleSheet.create({
   $largeContainerSize: imageWidth,
   $largeImageSize: imageWidth / 2,
+  $smallContainerSize: imageWidth / 2,
+  smallImageSize: imageWidth / 4,
   container: {
     alignItems: "center"
   },
