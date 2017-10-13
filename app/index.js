3861820e97307f9468f32586ce24f75e7c91c7b6
Thomas Johnson <tjohnson1106@gmail.com>
2017-10-13 17:47:43 -0400

    added list item currency view and checkmark to note selection

--- app/index.js
+++ app/index.js
@@ -11,7 +11,8 @@ EStyleSheet.build({
   $white: "#FFFFFF",
   $border: "#E2E2E2",
   $inputText: "#797979",
-  $lightGray: "#F0F0F0"
+  $lightGray: "#F0F0F0",
+  $darkText: "#343434"
 });
 
 export default () => <CurrencyList />;
